a1173747d4d08ddb9d228f6e9b1c587bb7c39c0a
marangoz <marangoz@gladsquid.com>
2020-10-04 16:40:55 +0100

    extend log parser, update rubocop

--- lib/parser/log_file.rb
+++ lib/parser/log_file.rb
@@ -1,16 +1,32 @@
 # frozen_string_literal: true
 
 module Parser
-  # Initialize and validates log file
+  # Initialize and parse log file
   class LogFile
     class FileMissing < StandardError; end
 
+    attr_reader :views, :errors
+
     def initialize(log_path)
       @log_path = log_path
+      @errors = []
+      @views = []
     end
 
-    def file_exist?
-      raise FileMissing, 'File is missing' unless ::File.exist?(@log_path)
+    def parse
+      ::File.readlines(@log_path).each do |row|
+        view = View.new(row)
+        view.validate!
+        @views << view
+      rescue View::InvalidRow => e
+        @errors << e.message
+      end
+    end
+
+    def parse!
+      raise(FileMissing, 'File is missing!') unless ::File.exist?(@log_path)
+
+      parse
     end
   end
 end
--- spec/parser/log_file_spec.rb
+++ spec/parser/log_file_spec.rb
@@ -4,21 +4,52 @@ require_relative '../spec_helper'
 
 RSpec.describe Parser::LogFile do
   let(:log_file) do
-    Parser::LogFile.new('/path')
+    described_class.new('/path')
   end
 
-  describe '#file_exist?' do
+  let(:log_file_rows) do
+    ["/about 802.683.925.780\n",
+     "/about/2 543.910.244.929\n"]
+  end
+
+  before do
+    allow(File).to receive(:readlines).and_return(log_file_rows)
+  end
+
+  describe '#parse' do
+    context 'when all rows are valid' do
+      it 'returns Rows' do
+        log_file.parse
+        expect(log_file.views.count).to eq 2
+      end
+    end
+
+    context 'when some rows are invalid' do
+      let(:log_file_rows) do
+        ["/about 802.683.925.780\n",
+         "/about/2\n"]
+      end
+
+      it 'returns Rows' do
+        log_file.parse
+        expect(log_file.views.count).to eq 1
+        expect(log_file.errors).to eq ["Row: /about/2\n is invalid!"]
+      end
+    end
+  end
+
+  describe '#parse!' do
     context 'when file exist' do
       it 'return true' do
         allow(File).to receive(:exist?).and_return(true)
 
-        expect { log_file.file_exist? }.not_to raise_error
+        expect { log_file.parse! }.not_to raise_error
       end
     end
 
     context 'when file missing' do
       it 'return raise FileMissing' do
-        expect { log_file.file_exist? }.to raise_error(Parser::LogFile::FileMissing)
+        expect { log_file.parse! }.to raise_error(Parser::LogFile::FileMissing)
       end
     end
   end
